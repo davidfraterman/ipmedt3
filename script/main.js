5231b47670c071e603d302ab59122dcf6b98c91d
David <davidfraterman@gmail.com>
2022-01-20 12:58:25 +0100

    cursor, component test, grid weg

--- script/main.js
+++ script/main.js
@@ -1,3 +1,15 @@
+// js check
 window.onload = () => {
-    console.log('JS Loaded');
+  console.log("Main.js loaded");
 };
+
+// rocket-ship test component
+AFRAME.registerComponent("rocket-ship", {
+  events: {
+    //   verander kleur als raket wordt geklikt met cursor
+    click: function () {
+      console.log("This entity was clicked!");
+      this.el.setAttribute("material", "color", "red");
+    },
+  },
+});
