2f21157e4e75322795cbb4348cce51fcaaddee68
David <davidfraterman@gmail.com>
2022-01-20 20:03:16 +0100

    test met text op scherm bij knop klik

--- script/main.js
+++ script/main.js
@@ -1,6 +1,8 @@
 // js check
 window.onload = () => {
   console.log("Main.js loaded");
+  const text = document.getElementById("text");
+  console.log(text);
 };
 
 // rocket-ship test component
@@ -17,10 +19,11 @@ AFRAME.registerComponent("rocket-ship", {
 // red button
 AFRAME.registerComponent("red-button", {
   events: {
-    //   verander kleur als raket wordt geklikt met cursor
+    //   remove text on click (test)
     click: function () {
       console.log("This entity was clicked!");
       this.el.setAttribute("position", "0 0.95 0.2");
+      text.setAttribute("visible", false);
       setTimeout(() => {
         this.el.setAttribute("position", "0 1 0.2");
       }, 500);
