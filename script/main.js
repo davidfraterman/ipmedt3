915844c02966310067872237f0367689d88670b1
David <davidfraterman@gmail.com>
2022-01-21 18:56:41 +0100

    cleanup + gltf conversie

--- script/main.js
+++ script/main.js
@@ -4,29 +4,3 @@ window.onload = () => {
   const text = document.getElementById("text");
   console.log(text);
 };
-
-// rocket-ship test component
-AFRAME.registerComponent("rocket-ship", {
-  events: {
-    //   verander kleur als raket wordt geklikt met cursor
-    click: function () {
-      console.log("This entity was clicked!");
-      this.el.setAttribute("material", "color", "red");
-    },
-  },
-});
-
-// red button
-AFRAME.registerComponent("red-button", {
-  events: {
-    //   remove text on click (test)
-    click: function () {
-      console.log("This entity was clicked!");
-      this.el.setAttribute("position", "0 0.95 0.2");
-      text.setAttribute("visible", false);
-      setTimeout(() => {
-        this.el.setAttribute("position", "0 1 0.2");
-      }, 500);
-    },
-  },
-});
