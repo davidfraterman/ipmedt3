de79edfd2b6fafb83fa609532f2645ce4646e222
David <davidfraterman@gmail.com>
2022-01-21 20:38:40 +0100

    gltf updates + gaze based walking + tl buizen

--- script/main.js
+++ script/main.js
@@ -1,6 +1,18 @@
-// js check
-window.onload = () => {
-  console.log("Main.js loaded");
-  const text = document.getElementById("text");
-  console.log(text);
+// Gaze based walking
+window.onload = function () {
+  let camera = document.getElementById("camera");
+  let destinations = document.getElementsByClassName("destinations");
+
+  for (var i = 0; i < destinations.length; i++) {
+    const destination = destinations[i];
+    destination.onclick = (event) => {
+      console.log(event.target);
+      // camera.setAttribute("position", event.target.getAttribute("position"));
+      let att = document.createAttribute("animation");
+      att.value = `property: position; easing: linear; dur: 1000; to: ${
+        event.target.getAttribute("position").x
+      } 0 ${event.target.getAttribute("position").z}`;
+      camera.setAttribute("animation", att.value);
+    };
+  }
 };
