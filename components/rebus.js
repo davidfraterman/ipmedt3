3aed03b8c75c3c38a939f5da22c62748b3c5b86e
DennisHSLeiden <92793860+DennisHSLeiden@users.noreply.github.com>
2022-01-25 22:16:41 +0100

    al het plaatsen van de rebus hints succesvol gemaakt dmv components

--- components/rebus.js
+++ components/rebus.js
@@ -5,7 +5,9 @@ window.onload = function () {
     temphold = "";
     let hold = null;
 
-    AFRAME.registerComponent('pickuprebusparts', {
+
+    // To pick up rebus models
+      AFRAME.registerComponent('pickuprebusmodel', {
         schema: {
         },
     
@@ -29,8 +31,36 @@ window.onload = function () {
             });
         }   
       });
+      
+      // To pick up rebus text hints
+      AFRAME.registerComponent('pickuprebustext', {
+        schema: {
+        },
     
-      AFRAME.registerComponent('placeintoplaceholder', {
+        init: function () {
+          
+          var el = this.el;  
+    
+            el.addEventListener('click', function () {
+             // if there is no current hold, set hold to the clicked element
+                if(hold == null){
+                  temp = el;
+                  temphold = temp.outerHTML;
+                  camera.innerHTML +=
+                  '<a-sphere id="js--hold" material="transparent: true; opacity: 0" position="-2.8 -2.3 -2" radius="0.5">' +
+                  temphold +
+                  "</a-sphere>";
+                  this.remove();
+                  hold = "text";
+                
+                }   
+            });
+        }   
+      });
+      
+      
+      // To place rebus models into placeholders
+      AFRAME.registerComponent('placeintomodelholder', {
         schema: {
         },
         init: function () {
@@ -44,16 +74,86 @@ window.onload = function () {
                 sphere.setAttribute("material", "transparent: true; opacity: 0");
                 sphere.setAttribute("position", {
                   x: -this.getAttribute("position").x,
-                  y: 2.1,
-                  z: -6,
+                  y: -this.getAttribute("position").z-1.4,
+                  z: -this.getAttribute("position").y+0.7,
                 });
                 sphere.innerHTML += temphold;
                 scene.appendChild(sphere);
                 document.getElementById("js--hold").remove();
                 hold = null;
+                temphold=""
               }
             });
         }
       });
+
+      AFRAME.registerComponent('placeintotextholder', {
+        schema: {
+        },
+        init: function () {
     
-}
+          var el = this.el;
+    
+            el.addEventListener('click', function() {
+             // if the hold is a text, place it in the text placeholder
+              if (hold == "text"){
+                let sphere = document.createElement("a-sphere");
+                sphere.setAttribute("material", "transparent: true; opacity: 0");
+                sphere.setAttribute("position", {
+                  x: -this.getAttribute("position").x-2.8,
+                  y: -this.getAttribute("position").z-1.8,
+                  z: -this.getAttribute("position").y+0.5, //change this to get out of wall --> + = out
+                });
+                sphere.innerHTML += temphold;
+                scene.appendChild(sphere);
+                document.getElementById("js--hold").remove();
+                hold = null;
+                temphold=""
+              }
+            });
+        }
+      });
+
+      // To place Any into the table
+      AFRAME.registerComponent('placeintotable', {
+        schema: {
+        },
+        init: function () {
+    
+          var el = this.el;
+    
+            el.addEventListener('click', function() {
+             // if the hold is a model place it on table with correct position
+              if (hold == "model"){
+                let sphere = document.createElement("a-sphere");
+                sphere.setAttribute("material", "transparent: true; opacity: 0");
+                sphere.setAttribute("position", {
+                  x: -this.getAttribute("position").x,
+                  y: -this.getAttribute("position").z-1,
+                  z: -this.getAttribute("position").y,
+                });
+                sphere.innerHTML += temphold;
+                scene.appendChild(sphere);
+                document.getElementById("js--hold").remove();
+                hold = null;
+                temphold=""
+              }
+                           // if the hold is a text place it on table with correct position
+              if (hold == "text"){
+                let sphere = document.createElement("a-sphere");
+                sphere.setAttribute("material", "transparent: true; opacity: 0");
+                sphere.setAttribute("position", {
+                  x: -this.getAttribute("position").x-2.8,
+                  y: -this.getAttribute("position").z-1.4,
+                  z: -this.getAttribute("position").y,
+                });
+                sphere.innerHTML += temphold;
+                scene.appendChild(sphere);
+                document.getElementById("js--hold").remove();
+                hold = null;
+                temphold=""
+              }
+            })
+          }
+      })
+    }
