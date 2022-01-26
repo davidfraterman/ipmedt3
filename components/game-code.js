dfe72d68754342222f18721920e44f82ff615936
David <davidfraterman@gmail.com>
2022-01-26 12:53:53 +0100

    code format

--- components/game-code.js
+++ components/game-code.js
@@ -1,16 +1,16 @@
 // game-code generator
 AFRAME.registerComponent("game-code", {
-  schema: {},
-  init: function () {
-    // 5 random digits voor de code, String zodat je altijd 5 digits hebt
-    const gameCode =
-      String(Math.floor(Math.random() * 9)) +
-      String(Math.floor(Math.random() * 9)) +
-      String(Math.floor(Math.random() * 9)) +
-      String(Math.floor(Math.random() * 9)) +
-      String(Math.floor(Math.random() * 9));
+    schema: {},
+    init: function () {
+        // 5 random digits voor de code, String zodat je altijd 5 digits hebt
+        const gameCode =
+            String(Math.floor(Math.random() * 9)) +
+            String(Math.floor(Math.random() * 9)) +
+            String(Math.floor(Math.random() * 9)) +
+            String(Math.floor(Math.random() * 9)) +
+            String(Math.floor(Math.random() * 9));
 
-    // zet gameCode in de html
-    this.el.setAttribute("text", { value: gameCode });
-  },
+        // zet gameCode in de html
+        this.el.setAttribute("text", { value: gameCode });
+    },
 });
