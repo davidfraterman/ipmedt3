cb39c5839878456610cdb73d7b8513edce7678c6
David <davidfraterman@gmail.com>
2022-01-27 14:38:46 +0100

    api fetch + button logic

--- components/load-rebus.js
+++ components/load-rebus.js
@@ -2,7 +2,20 @@ AFRAME.registerComponent("load-rebus", {
     schema: {},
 
     init: function () {
+        let data_holder = {};
+
         this.el.addEventListener("click", () => {
+            // Style (pressed)
+            this.el.setAttribute("color", "green");
+            this.el.setAttribute("height", "0.4");
+            setTimeout(() => {
+                this.el.setAttribute("height", "1");
+            }, 250);
+            setTimeout(() => {
+                this.el.setAttribute("color", "white");
+            }, 1500);
+
+            // API
             const api_base_url = "https://ipmedt3-5d5a0-default-rtdb.europe-west1.firebasedatabase.app/";
             const api_args = "game";
 
@@ -10,6 +23,11 @@ AFRAME.registerComponent("load-rebus", {
                 .then((response) => response.json())
                 .then((data) => {
                     console.log(data);
+                    data_holder = data;
+                })
+                .then(() => {
+                    // TRUE OF FALSE KOMT HIER UIT
+                    console.log(data_holder.image1.solved);
                 })
                 .catch((error) => {
                     console.error("Error:", error);
